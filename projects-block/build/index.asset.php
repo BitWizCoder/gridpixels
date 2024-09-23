18d3013843a99c6a65a6ab9a6b836875e7a76699
bitwizcoder <mhmdnoman01@gmail.com>
2024-09-23 12:58:32 +0600

    initiated new block

---
+++ projects-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '3ceb6d7bd51a882221a1');
---
+++ projects-block/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'd2a9810e9d4223adb96b');
