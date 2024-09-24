9afebfb5bd2f1f0c87ce858becd23dfae67a68e2
bitwizcoder <mhmdnoman01@gmail.com>
2024-09-24 20:31:48 +0600

    removed block

--- functions.php
+++ functions.php
@@ -47,3 +47,30 @@ function load_google_fonts()
     wp_enqueue_style('google-fonts', 'href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap', false);
 }
 add_action('wp_enqueue_scripts', 'load_google_fonts');
+
+
+// Custopm post types
+function create_custom_post_type()
+{
+    $labels = array(
+        'name'               => _x('Custom Content', 'post type general name'),
+        'singular_name'      => _x('Custom Block', 'post type singular name'),
+        'menu_name'          => _x('Custom Content', 'admin menu'),
+        'add_new_item'       => __('Add New Content'),
+        'edit_item'          => __('Edit Content'),
+        'all_items'          => __('All Content'),
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'has_archive'        => true,
+        'supports'           => array('title', 'editor', 'thumbnail'), // Enable title, editor, and featured image
+        'show_in_rest'       => true,  // IMPORTANT: Enables Gutenberg (block editor)
+        'hierarchical'       => false, // Set to true for hierarchical structure like pages
+        'menu_icon'          => 'dashicons-welcome-write-blog', // Optional: Adds a custom icon to the admin menu
+    );
+
+    register_post_type('custom_content', $args);
+}
+add_action('init', 'create_custom_post_type');
--- projects-block/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'd2a9810e9d4223adb96b');
