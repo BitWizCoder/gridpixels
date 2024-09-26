09e970b88206e0b85b723ef2029d5f5844da6224
bitwizcoder <mhmdnoman01@gmail.com>
2024-09-26 16:17:27 +0600

    updated home page

--- front-page.php
+++ front-page.php
@@ -1,7 +1,94 @@
 <?php get_header(); ?>
 
-<div id="content" class="site-content">
-    <h1 class="custom-headings">This is the front-page.php page</h1>
+<div id="homepage">
+    <div class="hero">
+        <h1>
+            Awesome Figma and Tailwind
+            <span>
+                <br>
+                Templates
+            </span>
+
+        </h1>
+        <p class="paragraph">
+            if you need help with for your project let us know. we will get back to you as soon as possible. we can help you with existing project or with new project.
+        </p>
+        <p class="link">
+            <a href="/">Home /</a>
+        </p>
+    </div>
+
+    <div class="cards">
+        <div class="card">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/image 1.jpg" alt="">
+            <h2>Lifestyle Landing page</h2>
+            <p>This is an elegant lifestyle landing page designed to inspire and engage. It combines beautiful imagery with a minimalist layout, creating a seamless browsing experience that resonates with the audience's aspirations.</p>
+            <div class="buttns">
+                <button class="btn">Figma <i class="fa-solid fa-download"></i></button>
+                <button class="btn">Tailwind <i class="fa-solid fa-download"></i></button>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/image 1.jpg" alt="">
+            <h2>Lifestyle Landing page</h2>
+            <p>This is an elegant lifestyle landing page designed to inspire and engage. It combines beautiful imagery with a minimalist layout, creating a seamless browsing experience that resonates with the audience's aspirations.</p>
+            <div class="buttns">
+                <button class="btn">Figma <i class="fa-solid fa-download"></i></button>
+                <button class="btn">Tailwind <i class="fa-solid fa-download"></i></button>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/image 1.jpg" alt="">
+            <h2>Lifestyle Landing page</h2>
+            <p>This is an elegant lifestyle landing page designed to inspire and engage. It combines beautiful imagery with a minimalist layout, creating a seamless browsing experience that resonates with the audience's aspirations.</p>
+            <div class="buttns">
+                <button class="btn">Figma <i class="fa-solid fa-download"></i></button>
+                <button class="btn">Tailwind <i class="fa-solid fa-download"></i></button>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/image 1.jpg" alt="">
+            <h2>Lifestyle Landing page</h2>
+            <p>This is an elegant lifestyle landing page designed to inspire and engage. It combines beautiful imagery with a minimalist layout, creating a seamless browsing experience that resonates with the audience's aspirations.</p>
+            <div class="buttns">
+                <button class="btn">Figma <i class="fa-solid fa-download"></i></button>
+                <button class="btn">Tailwind <i class="fa-solid fa-download"></i></button>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/image 1.jpg" alt="">
+            <h2>Lifestyle Landing page</h2>
+            <p>This is an elegant lifestyle landing page designed to inspire and engage. It combines beautiful imagery with a minimalist layout, creating a seamless browsing experience that resonates with the audience's aspirations.</p>
+            <div class="buttns">
+                <button class="btn">Figma <i class="fa-solid fa-download"></i></button>
+                <button class="btn">Tailwind <i class="fa-solid fa-download"></i></button>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/image 1.jpg" alt="">
+            <h2>Lifestyle Landing page</h2>
+            <p>This is an elegant lifestyle landing page designed to inspire and engage. It combines beautiful imagery with a minimalist layout, creating a seamless browsing experience that resonates with the audience's aspirations.</p>
+            <div class="buttns">
+                <button class="btn">Figma <i class="fa-solid fa-download"></i></button>
+                <button class="btn">Tailwind <i class="fa-solid fa-download"></i></button>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/image 1.jpg" alt="">
+            <h2>Lifestyle Landing page</h2>
+            <p>This is an elegant lifestyle landing page designed to inspire and engage. It combines beautiful imagery with a minimalist layout, creating a seamless browsing experience that resonates with the audience's aspirations.</p>
+            <div class="buttns">
+                <button class="btn">Figma <i class="fa-solid fa-download"></i></button>
+                <button class="btn">Tailwind <i class="fa-solid fa-download"></i></button>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php get_footer(); ?>
